1588062306b1d143120213221f08554c4db40ea5
Arjun0023 <arjunp0023@gmail.com>
2025-04-01 23:31:21 +0530

    remove unnecessary comment in Ask component

--- src/pages/Ask.jsx
+++ src/pages/Ask.jsx
@@ -11,7 +11,7 @@ import { useNavigate } from 'react-router-dom';
 import Navbar from '../components/navbar/Navbar';
 import InputArea from '../components/input/InputArea';
 
-// Memoize the result item component to prevent re-renders
+
 const ResultItem = React.memo(({ result, displayMode, darkMode, saveChartToDashboard, setDisplayMode, savedCharts, COLORS }) => {
     if (!result || !result.resultData) return null;
 
